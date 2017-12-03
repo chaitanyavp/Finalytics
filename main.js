bf99c10518922e3debddd0b30678ec8bb3458a34
chaitanyavp <chaitanya1998@gmail.com>
2017-12-03 03:15:52 -0500

    Fixed functionality

--- main.js
+++ main.js
@@ -6,9 +6,8 @@ var i = 0;
 var total = 0;
 
 function addDeal() {
-  if ( item.value !== '' && price.value !== '') {
-    dealsList.innerHTML += '<li id="' + i + '"><label class="control control--checkbox">' 
-    + item.value + "("+ link.value ")"+'<input type="checkbox"/><div class="control__indicator"></div></label> <span class="price">' + price.value + 'kn<button class="removeBtn" onclick="removeDeal(' + i + ')">x</button></span></li>';
+  if ( item.value !== '' && price.value !== '' && link.value !== '') {
+    dealsList.innerHTML += '<li id="' + i + '"><label class="control control--checkbox">' + item.value + "(" + link.value + ")" + '<input type="checkbox"/><div class="control__indicator"></div></label> <span class="price">' + price.value + '<button class="removeBtn" onclick="removeDeal(' + i + ')">x</button></span></li>';
     i += 1;
     item.value = "";
     price.value = "";
